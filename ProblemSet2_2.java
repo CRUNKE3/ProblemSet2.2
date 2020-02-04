71f01e49f13de4a1da7f5246f4fc38867df63ecb
Damian Czernikowski <dczernikowski1@ucvts.tec.nj.us>
2020-02-04 12:51:52 -0500

    kirby

--- ProblemSet2_2.java
+++ ProblemSet2_2.java
@@ -4,13 +4,19 @@ public class ProblemSet2_2 {
     public static void main(String[] args) {
         //Use print statements to create your own beautiful original ASCII art.
         //Use comments to describe what your image is depicting.
-        
-        
-        
-        
-        
-        
-        
-        
+        System.out.println("  XXXX                   ");
+        System.out.println("  XXXX                   ");
+        System.out.println("        ____________      ");
+        System.out.println("  XXXX              \     ");
+        System.out.println("  XXXX               \    ");
+        System.out.println("  XXXX                \   ");
+        System.out.println("                       )  ");
+        System.out.println("                      /  ");
+        System.out.println("                     /  ");
+        System.out.println("    \               /    ");
+        System.out.println("     \_____________/     ");
+        System.out.println("       XXXX   XXXX       ");
+        System.out.println("    XXXXXXXX XXXXXXXX    ");
+        System.out.println("     XXXXXX   XXXXXX     ");
     } 
 }
