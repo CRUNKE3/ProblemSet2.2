21fe938941a080b17aa97bf2ab6afeef2c4f1c2c
dczernikowski1 <dczernikowski1@BIH513-1913.ucvts.tec.nj.us>
2020-02-04 12:53:29 -0500

    kirby gang

--- ProblemSet2_2.java
+++ ProblemSet2_2.java
@@ -4,19 +4,19 @@ public class ProblemSet2_2 {
     public static void main(String[] args) {
         //Use print statements to create your own beautiful original ASCII art.
         //Use comments to describe what your image is depicting.
-        System.out.println("  XXXX                   ");
-        System.out.println("  XXXX                   ");
-        System.out.println("        ____________      ");
-        System.out.println("  XXXX              \     ");
-        System.out.println("  XXXX               \    ");
-        System.out.println("  XXXX                \   ");
-        System.out.println("                       )  ");
-        System.out.println("                      /  ");
-        System.out.println("                     /  ");
-        System.out.println("    \               /    ");
-        System.out.println("     \_____________/     ");
-        System.out.println("       XXXX   XXXX       ");
-        System.out.println("    XXXXXXXX XXXXXXXX    ");
-        System.out.println("     XXXXXX   XXXXXX     ");
+        System.out.println("                          ");
+        System.out.println("      XXXXXXXXXXXXX       ");
+        System.out.println("     X             X      ");
+        System.out.println("    X               X     ");
+        System.out.println("   X    X      X     X    ");
+        System.out.println("  X    X  XXXX  X     X   ");
+        System.out.println(" X        X  X         X  ");
+        System.out.println("  X        XX         X   ");
+        System.out.println("  XX                 XX   ");
+        System.out.println("  XXX               XXX   ");
+        System.out.println("     XXXXXXXXXXXXXXXX      ");
+        System.out.println("       XXXX   XXXX        ");
+        System.out.println("    XXXXXXXX XXXXXXXX     ");
+        System.out.println("     XXXXXX   XXXXXX      ");
     } 
 }
